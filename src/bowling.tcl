2ec52872db3e3f187cf965d5dbefae69ce967bb0
Carlos Gomez <cgomezv107@gmail.com>
2015-10-11 15:19:26 -0600

    Testing score for four throws without any strike or sparse and getting the score per frame (first and second)

--- src/bowling.tcl
+++ src/bowling.tcl
@@ -1,4 +1,11 @@
-set score [ expr {0}]
+proc initialize_global_variables {} {
+    global score
+    global throws
+    global current_throw
+    set score [ expr {0}]
+    set current_throw 0
+    return
+}
 
 proc sum {a b} {
     return [expr {$a + $b}]
@@ -21,6 +28,23 @@ proc get_score {} {
 
 proc add_throw { pins } {
     global score
+    global throws
+    global current_throw
+    incr current_throw
+    set throws($current_throw) $pins
     set score [expr {$score + $pins}]
+    return [ expr {$score}]
+}
+
+proc get_score_for_frame { frame } {
+    global throws
+    set score 0
+    set ball 0
+    for {set current_frame 0} {$current_frame < $frame} {incr current_frame} { 
+	incr ball
+	set score [expr {$score + $throws($ball)}]
+	incr ball
+	set score [expr {$score + $throws($ball)}]
+    }
     return [ expr {$score}]
 }
