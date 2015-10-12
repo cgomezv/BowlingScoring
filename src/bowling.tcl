22e692bdb33d32424d5cd8efb7af35a25ac01ec4
Carlos Gomez <cgomezv107@gmail.com>
2015-10-12 15:08:11 -0600

    Some refactoring

--- src/bowling.tcl
+++ src/bowling.tcl
@@ -4,6 +4,9 @@ proc initialize_global_variables {} {
     global current_throw
     global current_frame
     global isFirstThrow
+    global ball
+    global firstThrow
+    global secondThrow
     set score [ expr {0}]
     set current_throw 0
     set current_frame 1
@@ -65,8 +68,29 @@ proc add_throw { pins } {
     adjust_current_frame $pins
 }
 
+proc handle_second_throw {} {
+    global throws
+    global ball
+    global firstThrow
+    global secondThrow
+    set score 0
+    incr ball
+    set secondThrow [expr {$throws($ball)}]
+    set frameScore [expr {$firstThrow + $secondThrow}]
+    if {$frameScore == 10} {
+	incr ball
+	set score [expr {$score + $frameScore + $throws($ball)}]
+	set ball [expr {$ball - 1}]
+    } else {
+	set score [expr {$score + $frameScore}]
+    }
+    return [ expr {$score}]
+}
 proc get_score_for_frame { frame } {
     global throws
+    global ball
+    global firstThrow
+    global secondThrow
     set score 0
     set ball 0
     for {set current_frame 0} {$current_frame < $frame} {incr current_frame} {
@@ -80,16 +104,8 @@ proc get_score_for_frame { frame } {
 	    set score [expr {$score + $firstThrow + $nextThrow + $nextNextThrow}]
 	    set ball [expr {$ball - 2}]
 	} else {
-	    incr ball
-	    set secondThrow [expr {$throws($ball)}]
-	    set frameScore [expr {$firstThrow + $secondThrow}]
-	    if {$frameScore == 10} {
-		incr ball
-		set score [expr {$score + $frameScore + $throws($ball)}]
-		set ball [expr {$ball - 1}]
-	    } else {
-		set score [expr {$score + $frameScore}]
-	    }
+	    set second_throw [handle_second_throw]
+	    set score [expr {$score + $second_throw}]
 	}
     }
     return [ expr {$score}]
